9f6b7011cde75c709e920443d45f32e700a85363
Arshad <arshadcsinfo@gmail.com>
2025-12-15 03:55:08 +0530

    removed region from peering.tf

--- terraform/envs/us-east-1/peering.tf
+++ terraform/envs/us-east-1/peering.tf
@@ -3,7 +3,6 @@ module "vpc_peering" {
 
   requester_vpc_id       = module.vpc_gateway.vpc_id
   accepter_vpc_id        = module.vpc_backend.vpc_id
-  region                 = "us-east-1"
   gateway_private_rtb_id = module.vpc_gateway.private_route_table_id
   backend_private_rtb_id = module.vpc_backend.private_route_table_id
   gateway_cidr           = module.vpc_gateway.cidr
