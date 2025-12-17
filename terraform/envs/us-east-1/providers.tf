d55723af121065d09645a6ff976894862431b58d
Arshad <arshadcsinfo@gmail.com>
2025-12-17 20:39:51 +0530

    Create aws-auth ConfigMap directly in terraform for guaranteed kubectl access

--- terraform/envs/us-east-1/providers.tf
+++ terraform/envs/us-east-1/providers.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+provider "kubernetes" {
+  host                   = module.eks_backend.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_backend.cluster_certificate_authority_data)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = ["eks", "get-token", "--cluster-name", module.eks_backend.cluster_name, "--region", "us-east-1"]
+  }
+}
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -28,3 +28,33 @@ module "eks" {
     }
   }
 }
+
+# Create aws-auth ConfigMap directly in terraform to ensure GitHub Actions user has access
+resource "kubernetes_config_map_v1" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = module.eks.eks_managed_node_groups.default.iam_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups = [
+          "system:bootstrappers",
+          "system:nodes"
+        ]
+      }
+    ])
+    mapUsers = yamlencode([
+      {
+        userarn  = "arn:aws:iam::721500739616:user/arshadcsinfo@gmail.com"
+        username = "github-actions"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  depends_on = [module.eks.cluster_name]
+}
