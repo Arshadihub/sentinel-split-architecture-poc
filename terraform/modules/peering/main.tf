5165b8e745d012cfb309a22d38f74d64add2c1a4
Arshad <arshadcsinfo@gmail.com>
2025-12-18 19:48:14 +0530

    Fix: Ensure EKS nodes use only private subnets

--- terraform/modules/peering/main.tf
+++ terraform/modules/peering/main.tf
@@ -7,16 +7,16 @@ resource "aws_vpc_peering_connection" "peer" {
 
 # Add route from gateway to backend in gateway private route tables
 resource "aws_route" "gateway_to_backend" {
-  for_each                  = toset(var.gateway_private_rtb_ids)
-  route_table_id            = each.value
+  count                     = length(var.gateway_private_rtb_ids)
+  route_table_id            = var.gateway_private_rtb_ids[count.index]
   destination_cidr_block    = var.backend_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
 # Add route from backend to gateway in backend private route tables
 resource "aws_route" "backend_to_gateway" {
-  for_each                  = toset(var.backend_private_rtb_ids)
-  route_table_id            = each.value
+  count                     = length(var.backend_private_rtb_ids)
+  route_table_id            = var.backend_private_rtb_ids[count.index]
   destination_cidr_block    = var.gateway_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
